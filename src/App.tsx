550fbde559023777d913fb4cefa91cf0bf43fdd9
Gleb Kodrik <gkodrik@IBS.ru>
2023-12-02 02:12:43 +0300

    обновил колонки

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,51 @@ import { makeData } from './components/utils/makeData';
 const columnHelper = createColumnHelper<any>();
 const columns = [
   {
-    accessorKey: 'firstName',
-    id: 'firstName',
-    header: 'Имя',
-    cell: (info) => info.getValue(),
-  },
-  {
-    id: 'lastName',
-    header: 'few',
+    header: 'Name',
+    footer: (props) => props.column.id,
     columns: [
       {
-        accessorKey: 'age',
-        id: 'ag3e',
-        header: 'Количество годов',
+        accessorKey: 'firstName',
+        cell: (info) => info.getValue(),
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorFn: (row) => row.lastName,
+        id: 'lastName',
+        cell: (info) => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: (props) => props.column.id,
       },
     ],
   },
   {
-    accessorKey: 'visits',
-    id: 'visits',
-    header: 'Визит',
+    header: 'Info',
+    footer: (props) => props.column.id,
     columns: [
       {
-        accessorKey: 'status',
-        id: 'status',
-        header: 'Статус по жизни',
+        accessorKey: 'age',
+        header: () => 'Age',
         footer: (props) => props.column.id,
+      },
+      {
+        header: 'More Info',
+        columns: [
+          {
+            accessorKey: 'visits',
+            header: () => <span>Visits</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'status',
+            header: 'Status',
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorKey: 'progress',
+            header: 'Profile Progress',
+            footer: (props) => props.column.id,
+          },
+        ],
       },
     ],
   },
